68997236974be89d62c969ebbfe2341659b0a43f
Arseni Pazniak <pznkcommercial@gmail.com>
2023-04-26 12:33:44 +0300

    feat: webpack config **

--- calculator/config-overrides.js
+++ calculator/config-overrides.js
@@ -1,17 +1,22 @@
-const { alias } = require('react-app-rewire-alias');
-
-const aliasMap = {
-  '@components': 'src/components',
-  '@constants': 'src/constants',
-  '@pages': 'src/pages',
-  '@store': 'src/store',
-  '@styles': 'src/styles',
-  '@utils': 'src/utils',
-};
+const path = require('path');
 
 module.exports = function override(config, env) {
-  alias(aliasMap)(config);
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@components': path.resolve(__dirname, './src/components'),
+    '@constants': path.resolve(__dirname, './src/constants'),
+    '@pages': path.resolve(__dirname, './src/pages'),
+    '@store': path.resolve(__dirname, './src/store'),
+    '@styles': path.resolve(__dirname, './src/styles'),
+    '@utils': path.resolve(__dirname, './src/utils'),
+  };
+
+  config.output = {
+    // ЗДЕСЬ МОЖНО ДОБАВИТЬ КОНФИГУРАЦИЮ WEBPACK
+    ...config.output,
+    path: path.resolve(__dirname, './dist'),
+    filename: 'modsen_bundle.js',
+  };
+
   return config;
 };
-
-module.exports = alias(aliasMap);
